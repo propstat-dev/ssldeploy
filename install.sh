e20168e4a6152ab9ea4a927ef3c2c91d5261ca97
PropStat DEV Team <info@propstat.net>
2026-05-20 23:54:33 +0200

    Update install with warnings.

--- install.sh
+++ install.sh
@@ -1,24 +1,110 @@
-# Logo
-cat << "EOF" 
+#!/bin/bash
+# ==========================================
+# Variables
+# ==========================================
+RED=$'\e[0;31m'
 
-▗▄▄▖ ▗▄▄▖  ▗▄▖ ▗▄▄▖  ▗▄▄▖▗▄▄▄▖▗▄▖▗▄▄▄▖
-▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌     █ ▐▌ ▐▌ █  
-▐▛▀▘ ▐▛▀▚▖▐▌ ▐▌▐▛▀▘  ▝▀▚▖  █ ▐▛▀▜▌ █  
-▐▌   ▐▌ ▐▌▝▚▄▞▘▐▌   ▗▄▄▞▘  █ ▐▌ ▐▌ █  
-                                      
-Welcome to the Propstat (c) SSL Deploy Installer. 
-All rights to the Propstat Logo and Name reserved.
-This tool is provided with an open source or source accessible license depending on your use case.
-Please verify which license applies on our Github Repository:
-https://github.com/propstat/ssldeploy
+# ==========================================
+# LOGO
+# ==========================================
+cat << "EOF"
+
+           il*%%%%*             i%%%%%%%           
+          iiiI*%%%%%          .iii*%%%%*%          
+         iiiiii%%%%%%▒       .iiiii%*%%%*%         
+        iiiiiii %%%%%%<      iiiiii %*%%%%%        
+       iiiiiii   *%%%%%%   ;iiiiii   **%%%%*       
+      iiiiiii     %%%%%*% ;iiiiii     %*%%%%*      
+     iiiiiii       **%%%**iiiiii       *%%%%*%     
+    iiiiiii         *%%%%*<iiii         **%%%**    
+   iiiiiii           %*%%%**ii           *%%%%*%   
+  iiiiiiiiiiiiiiiiiiii%%%%%%*%%%%%%%%%%%%%%%%%%%*  
+ iiiiiiiiiiiiiiiiiiiiii%*%%%%%%%%%%%%%%%%%%%%%%%%* 
+iiiiiiiiiiiiiiiiiiiiiiii**%%%%%%%%%%%%%%%%%%%%%%%%%
+                                                   
+iiiii iiiiii iiiiii  iiiii iiiii:iiiiii  iii iiiiii
+iiiiiiii  iiiii  iii ii:iiiiiiii   ii   iiii   ii  
+iiiii iiiiiiiii  iii iiiii iiiiii  ii  iiiiii  ii  
+ii    ii iii iiiiii  ii    iiiiii  ii iiiiiiii ii  
+
 EOF
 
-# Dependencies
-echo "Do you wish to proceed installing this software?"
-select strictreply in "Yes" "No"; do
-    relaxedreply=${strictreply:-$REPLY}
-    case $relaxedreply in
-        Yes | yes | y ) exit; break;;
-        No  | no  | n ) exit;;
+# ==========================================
+# Copyright & License Warning
+# ==========================================
+
+date +"Copyright 2026 - YYYY by Propstat"
+echo "Non commercial use is free of charge, please consult our license for commercial use."
+echo "The license is available at https://github.com/propstat/ssldeploy"
+
+# ==========================================
+# Accept License
+# ==========================================
+
+while true; do
+    echo "Do you accept the license?"
+    echo "Type Y to continue or N to cancel the install."
+    read -p "#? " reply
+
+    case $reply in
+        1 | Yes | yes | y | Y ) 
+            make install
+            break
+            ;;
+        2 | No | no | n | N ) 
+            echo "Installation aborted."
+            exit 0
+            ;;
+        * ) 
+            echo "Invalid option. Please try again."
+            echo ""
+            ;;
+    esac
+done
+
+# ==========================================
+# Introduction and Warning
+# ==========================================
+
+cat << "EOF"
+This tool allows you to request and deploy Let’s Encrypt™ Certificates among your services.
+
+Currently following DNS providers are supported:
+- Cloudflare
+
+If you want to introduce support for an additional provider visit our repository at
+https://github.com/propstat/ssldeploy
+EOF
+cat << EOF
+${RED}==========================================${NC}
+${RED}WARNING${NC}
+${RED}==========================================${NC}
+${RED}At the end of this process, this machine will hold credentials that allow to modify your DNS zone files. This server should not be accessible from the public.${NC}
+${RED}The software is provided "as is," meaning the original authors are not liable for any damages or bugs.${NC}
+${RED}Proceed only if you know what you are doing.${NC}
+
+Requirements:
+1) This machine needs access to the internet to validate your certificate request.
+2) You will subsequently need credentials to access the servers you want to deploy certificates on.
+EOF
+
+while true; do
+    echo "Do you understand the risks and want to proceed?"
+    echo "Type Y to continue or N to cancel the install."
+    read -p "#? " reply
+
+    case $reply in
+        1 | Yes | yes | y | Y ) 
+            make install
+            break
+            ;;
+        2 | No | no | n | N ) 
+            echo "Installation aborted."
+            exit 0
+            ;;
+        * ) 
+            echo "Invalid option. Please try again."
+            echo ""
+            ;;
     esac
 done
