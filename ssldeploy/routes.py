5c523fcec1efbf2a77de92c139df1e8115bae542
PROPSTAT-DEV <info@propstat.net>
2026-06-01 18:19:31 +0200

    Basic Log-In Page

--- ssldeploy/routes.py
+++ ssldeploy/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from ssldeploy import ssldeploy
 
 @ssldeploy.route('/')
 @ssldeploy.route('/index')
 def index():
-    return "SSL Deploy Flask Test"
+    brand = {'brandname' : 'company'}
+    return render_template('index.html', title='SSL Deploy Admin Log-In', brand=brand)
