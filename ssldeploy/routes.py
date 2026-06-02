669e7954a1364083bbf61632e6e1ab30379bd7bb
PROPSTAT-DEV <info@propstat.net>
2026-06-02 09:04:25 +0200

    Separated Log-In page for users and PAM
    Added Log-In pages for Admin and Self Servicing

--- ssldeploy/routes.py
+++ ssldeploy/routes.py
@@ -4,5 +4,10 @@ from ssldeploy import ssldeploy
 @ssldeploy.route('/')
 @ssldeploy.route('/index')
 def index():
-    brand = {'brandname' : 'company'}
-    return render_template('index.html', title='SSL Deploy Admin Log-In', brand=brand)
+    brand = {'brandname' : 'companyname'}
+    return render_template('selfservice/userlogin.html', title='SSL Deploy Self Service', brand=brand)
+
+@ssldeploy.route('/admin/')
+def admin():
+    brand = {'brandname' : 'companyname'}
+    return render_template('admin/adminlogin.html', title='SSL Deploy Admin Interface', brand=brand)
