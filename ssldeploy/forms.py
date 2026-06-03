ef79e4c95dceb0d195f04fdf09a499a1572751a2
PROPSTAT-DEV <info@propstat.net>
2026-06-03 13:23:55 +0200

    Fix Email Validation on Log-In Form

--- ssldeploy/forms.py
+++ ssldeploy/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class UserLoginForm(FlaskForm):
